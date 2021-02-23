d743ffea270bd66b1a1dec716e466fc3944ff2b1
CrystalBRana <crystalkoinch@gmail.com>
2021-02-23 09:42:58 +0545

    area of triangle

--- pythonlab/area_of_triangle.py
+++ pythonlab/area_of_triangle.py
@@ -1,1 +1,8 @@
-#Write a program that reads the length
+# Write a program that reads the length of the base and height of the right angled triangle and prints the area.
+# Every number is given on a separate line.
+length = int(input('Enter the value of length:'))
+height = int(input('Enter the value of height:'))
+area = (length * height)//2
+# the floor division // rounds the result down to the nearest whole number
+print(f'The area of right-angled triangle is{area}')
+
