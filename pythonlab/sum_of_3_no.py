293604f579308ebb41ba18fc8e996929bd78521f
CrystalBRana <crystalkoinch@gmail.com>
2021-02-23 09:35:56 +0545

    sum of 3 numbers

--- pythonlab/sum_of_3_no.py
+++ pythonlab/sum_of_3_no.py
@@ -1,3 +1,4 @@
+#write a program that takes three numbers and print their sum. Every number is given on a separate line.
 a=int(input('Enter the first number:'))
 b=int(input('Enter the second number:'))
 c=int(input('Enter the third number:'))
